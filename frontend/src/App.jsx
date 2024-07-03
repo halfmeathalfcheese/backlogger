a514557ce715c06fb5e85b09f02275aaf0ffe6a7
Tim Pham <z5361150@unsw.edu.au>
2024-07-04 02:39:28 +1000

    Added register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,31 @@
 import './App.scss';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import Main from './Main';
 
+function HeaderWithLocation() {
+  const location = useLocation();
+  const hideHeaderOn = ['/login', '/register'];
+
+  return !hideHeaderOn.includes(location.pathname) && (
+    <header>
+      <Header />
+    </header>
+  );
+}
+
 function App() {
   return (
-      <div className="App">
-        <BrowserRouter>
-          <header>
-            <Header />
-          </header>
-          <main>
-            <Main />
-          </main>
-        </BrowserRouter>
-      </div>
+    <div className="App">
+      <BrowserRouter>
+        <HeaderWithLocation />
+        <main>
+          <Main />
+        </main>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -3,13 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 
 import LandingPage from "./pages/LandingPage";
 import SearchPage from "./pages/SearchPage";
+import RegisterPage from "./pages/RegisterPage";
+import GamePage from "./pages/GamePage";
 
 const Main = () => {
   return (
       <div className="Main">
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/search/:query" element={<SearchPage />} />
+          <Route path="/game/:id" element={<GamePage />} />
         </Routes>
       </div>
   );
